94f0e3681e7676804e786530739ae6f9ff72eac3
OUSSAMA <oussama@OUSSAMAs-iMac.local>
2022-06-14 21:29:59 +0100

    hello world

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "myheader.h"
+
+int	main(void)
+{
+	char	*name;
+	char	c;
+	int	nb;
+
+	name = "oussama";
+	c = 'A';
+	nb = 23039;
+	ft_putstr("Hello World");
+	ft_putchar('\n');
+	ft_putchar(c);
+	ft_putchar('\n');
+	ft_putnbr(nb);
+	return (0);
+}
